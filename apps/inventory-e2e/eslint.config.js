0823a96bd0cee6730623ffed76db2b346de385eb
drwla <ilyarredmi@gmail.com>
2024-12-24 14:54:51 +0500

    cmt 3sa 32

--- apps/inventory-e2e/eslint.config.js
+++ apps/inventory-e2e/eslint.config.js
@@ -1,10 +1,10 @@
-const baseConfig = require('../eslint.base.config.js');
 const cypress = require('eslint-plugin-cypress/flat');
+const baseConfig = require('../../eslint.base.config.js');
 
 module.exports = [
-  ...baseConfig,
-
   cypress.configs['recommended'],
+
+  ...baseConfig,
   {
     // Override or add rules here
     rules: {},
